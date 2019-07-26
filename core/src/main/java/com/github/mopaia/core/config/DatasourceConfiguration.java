235a9a99c7153f466856823dcef4b27efedc8684
Danilo Perassoli de Souza <danilo.souza@ifood.com.br>
2019-07-26 16:04:17 -0300

    configure postgres conn

---
+++ core/src/main/java/com/github/mopaia/core/config/DatasourceConfiguration.java
@@ -0,0 +1,37 @@
+package com.github.mopaia.core.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.datasource.hikari")
+public class DatasourceConfiguration extends HikariConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new HikariDataSource(this);
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        return new NamedParameterJdbcTemplate(jdbcTemplate);
+    }
+}
